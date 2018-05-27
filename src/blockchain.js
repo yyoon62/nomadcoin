bb8bdea83e05fa7052f6e6cedfcc42d8be33af7b
Young Yoon <yyoon62@gatech.edu>
2018-05-27 11:07:00 -0400

    added genesis block

--- src/blockchain.js
+++ src/blockchain.js
@@ -5,4 +5,17 @@ class Block {
     this.previousHash = previousHash;
     this.timestamp = timestamp;
     this.data = data;
+  }
 }
+
+const genesisBlock = new Block(
+  0,
+  "89885017C8C6F245BD5B52ED1C3E0CBA294DCCE78014DE17650777F6EBC62E73",
+  null,
+  1527430455015,
+  "this is genesis block!"
+)
+
+let blockchain = [genesisBlock]
+
+console.log(blockchain)
