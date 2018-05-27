6653019347f1d8697956f1cc36c9aba6dd8fec16
Young Yoon <young@Youngs-MacBook-Pro.local>
2018-05-27 09:55:42 -0400

    added package json file' '

---
+++ src/blockchain.js
@@ -0,0 +1,8 @@
+class Block {
+  constructor(index, hash, previousHash, timestamp, data){
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.timestamp = timestamp;
+    this.data = data;
+}
